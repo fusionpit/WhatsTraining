6d27f71e7cba5a3a0236f2242554f6ce6551a05d
fusionpit <fusionpit@gmail.com>
2026-02-22 14:45:03 -0700

    weapon skills

---
+++ Classes/WeaponSkills.lua
@@ -0,0 +1,19 @@
+local _, wt = ...
+
+wt.WeaponSkills = {
+    [196] = {classes = {"WARRIOR", "PALADIN", "HUNTER", "SHAMAN"}}, -- One-Handed Axes
+    [197] = {classes = {"WARRIOR", "PALADIN", "HUNTER", "SHAMAN"}}, -- Two-Handed Axes
+    [198] = {classes = {"WARRIOR", "PALADIN", "ROGUE", "PRIEST", "SHAMAN", "DRUID"}}, -- One-Handed Maces
+    [199] = {classes = {"WARRIOR", "PALADIN", "SHAMAN", "DRUID"}}, -- Two-Handed Maces
+    [200] = {classes = {"WARRIOR", "PALADIN", "HUNTER", "DRUID"}, level = 20, cost = 10000}, -- Polearms
+    [201] = {classes = {"WARRIOR", "PALADIN", "HUNTER", "ROGUE", "MAGE", "WARLOCK"}}, -- One-Handed Swords
+    [202] = {classes = {"WARRIOR", "PALADIN", "HUNTER"}}, -- Two-Handed Swords
+    [227] = {classes = {"WARRIOR", "HUNTER", "PRIEST", "SHAMAN", "DRUID", "WARLOCK", "MAGE"}}, -- Staves
+    [264] = {classes = {"WARRIOR", "HUNTER", "ROGUE"}}, -- Bows
+    [266] = {classes = {"WARRIOR", "HUNTER", "ROGUE"}}, -- Guns
+    [1180] = {classes = {"WARRIOR", "HUNTER", "ROGUE", "PRIEST", "SHAMAN", "DRUID", "WARLOCK", "MAGE"}}, -- Daggers
+    [2567] = {classes = {"WARRIOR", "HUNTER", "ROGUE"}}, -- Thrown
+    [5011] = {classes = {"WARRIOR", "HUNTER", "ROGUE"}}, -- Crossbows
+    [15590] = {classes = {"WARRIOR", "HUNTER", "ROGUE", "SHAMAN", "DRUID"}}, -- Fist Weapons
+}
+
