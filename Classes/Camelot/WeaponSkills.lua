68691703c936c6656312113c25ca8f921c6c728a
fusionpit <fusionpit@gmail.com>
2026-09-19 22:20:20 -0600

    added weapon skills

---
+++ Classes/Camelot/WeaponSkills.lua
@@ -0,0 +1,5 @@
+local _, wt = ...
+
+wt.WeaponSkills[196].classes.ROGUE = true -- One-Handed Axes
+wt.WeaponSkills[197].classes.SHAMAN = true -- Two-Handed Axes
+wt.WeaponSkills[199].classes.SHAMAN = true -- Two-Handed Maces
